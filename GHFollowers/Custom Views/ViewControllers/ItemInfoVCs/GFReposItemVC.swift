2a6ce6dcaf2e302da7b8818d630aba665ffc8a3d
eziors <marcos.barbosa@mail.uft.edu.br>
2024-04-05 00:40:33 -0300

    Adding ItemInfoVCSuperClass and SubClasses

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFReposItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFReposItemVC.swift
+//  GHFollowers
+//
+//  Created by Marcos Barbosa on 04/04/24.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
