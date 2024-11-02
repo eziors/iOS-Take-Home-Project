1b4890650b462f0b9bc85ea10c986d5d0614bcb9
eziors <marcos.barbosa@mail.uft.edu.br>
2024-11-02 00:35:14 -0300

    Fixing repo

---
+++ GHFollowers/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,29 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Marcos Barbosa on 18/02/24.
+//
+
+import UIKit
+import SafariServices
+
+
+
+extension UIViewController {
+        
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+    
+    func presentSafariVC(with url: URL) {
+        let safariVC = SFSafariViewController(url: url)
+        safariVC.preferredControlTintColor = .systemGreen
+        present(safariVC, animated: true)
+    }
+}
